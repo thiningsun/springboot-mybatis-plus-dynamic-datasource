76fbfcf9579304a2e3dc0ae24ba339e660a0d24a
zhangshiqiang01 <zhangshiqiang01@xinye.com>
2022-06-12 11:53:26 +0800

    mybatis-plus多数据源测试案例

---
+++ src/main/java/com/mp/demo/config/MybatisPlusConfig.java
@@ -0,0 +1,24 @@
+package com.mp.demo.config;
+
+import com.baomidou.mybatisplus.extension.plugins.OptimisticLockerInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * MybatisPlus配置类
+ * @Author Sans
+ * @CreateTime 2019/5/26 17:20
+ */
+@Configuration
+@MapperScan(basePackages = {"com.mp.**.dao"}) //扫描DAO
+public class MybatisPlusConfig {
+    /**
+     * 分页插件
+     */
+    @Bean
+    public PaginationInterceptor paginationInterceptor() {
+        return new PaginationInterceptor();
+    }
+}
